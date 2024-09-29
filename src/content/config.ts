7a6dc1709bc0eac6445c9e32970907ed9220025b
Sherif Hoti <sherifhoti1235@hotmail.com>
2024-09-29 20:17:55 +0200

    Added major md files and astro components

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from "astro:content";
+
+export const collections = {
+  tech: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()),
+      img: z.string(),
+      img_alt: z.string().optional(),
+    }),
+  }),
+  about: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z
+        .literal("backround")
+        .or(z.literal("education"))
+        .or(z.literal("skills")),
+    }),
+  }),
+};
